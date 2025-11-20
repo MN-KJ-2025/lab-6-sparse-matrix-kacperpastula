22299eae318c90a93117c1288579913c86fb6de3
kpastula <kacperpastula4@gmail.com>
2025-11-20 12:40:29 +0100

    funkcje z main zrobione, zadanie 3 oraz opisy i wnioski do zrobienia

--- main.py
+++ main.py
@@ -12,9 +12,17 @@ import scipy as sp
 
 
 def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
+
+    if not isinstance(A, (np.ndarray, sp.sparse.csc_array)):
+        return None
+    if A.ndim != 2 or A.shape[0] != A.shape[1]:
+        return None
     
-    sum = np.sum(A,1)
-    diagonal = np.diagolnal(A)
+    A = A.toarray() if sp.sparse.issparse(A) else A
+    A_abs = np.abs(A)
+    diag = np.diagonal(A_abs)
+    sum_rows = np.sum(A_abs, axis=1) - diag
+    return np.all(diag >= sum_rows)
 
     """Funkcja sprawdzająca czy podana macierz jest diagonalnie zdominowana.
 
@@ -31,6 +39,14 @@ def is_diagonally_dominant(A: np.ndarray | sp.sparse.csc_array) -> bool | None:
 
 
 def residual_norm(A: np.ndarray, x: np.ndarray, b: np.ndarray) -> float | None:
+    if not isinstance(A, np.ndarray) or not isinstance(x, np.ndarray) or not isinstance(b, np.ndarray):
+        return None
+    if A.ndim != 2 or x.ndim != 1 or b.ndim != 1:
+        return None
+    if A.shape[0] != b.shape[0] or A.shape[1] != x.shape[0]:
+        return None
+    return np.linalg.norm(A @ x - b)
+
     """Funkcja obliczająca normę residuum dla równania postaci: 
     Ax = b.
 
